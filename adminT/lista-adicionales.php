037b3c5448e601fc9d2b872bd1566910938fab3a
Feralram <zuvaro010@gmail.com>
2024-08-12 02:38:39 -0600

    bugs

--- adminT/lista-adicionales.php
+++ adminT/lista-adicionales.php
@@ -100,6 +100,14 @@ $cotizaciones = $usuario->obtenerCotizacionesAdicionales();
           </a>
         </li>
         <li class="nav-item">
+          <a class="nav-link text-white active bg-gradient-info" href="./lista-adicionales.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <span class="material-icons opacity-10">folder</span>
+            </div>
+            <span class="nav-link-text ms-1">Lista Adicionales</span>
+          </a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link text-white" href="./listaUnidades.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <span class="material-icons opacity-10">folder</span>
@@ -107,14 +115,7 @@ $cotizaciones = $usuario->obtenerCotizacionesAdicionales();
             <span class="nav-link-text ms-1">Lista Unidades</span>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link text-white" href="./listaOperadores.php">
-            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <span class="material-icons opacity-10">folder</span>
-            </div>
-            <span class="nav-link-text ms-1">Lista Operadores</span>
-          </a>
-        </li>
+
         <li class="nav-item">
           <a class="nav-link text-white" href="./lista-adicionales.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
